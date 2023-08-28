eab82dbf31b2822c780589de609d4a810e149456
salihabrik <briksaliha@gmail.com>
2023-08-28 23:36:35 +0200

    fix my code

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -8,8 +8,8 @@ def fizzbuzz(n):
     """
     FizzBuzz function prints numbers from 1 to n separated by a space.
 
-    - For multiples of three print "Fizz" instead of the number.
-    - For multiples of five print "Buzz".
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
     - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
@@ -31,8 +31,8 @@ def fizzbuzz(n):
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
-        print("Usage: ./fizzbuzz.py <number>")
-        print("Example: ./fizzbuzz.py 89")
+        print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
     number = int(sys.argv[1])
